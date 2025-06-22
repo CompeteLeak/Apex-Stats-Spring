f82b9fbb0ade1f69cdb79640aa0ffa087d0bcf6e
competeleak <malikiw20@gmail.com>
2025-06-22 10:13:55 -0400

    code clean ups

--- src/main/java/com/legends/apexapi/config/ApexApiConfig.java
+++ src/main/java/com/legends/apexapi/config/ApexApiConfig.java
@@ -1,8 +1,6 @@
 package com.legends.apexapi.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
 import lombok.Data;
 
 
